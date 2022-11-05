c8793771b49bd5197af45612b8867e6e62531cd9
ekolpakova <konanhayumi.kh@gmail.com>
2022-11-05 23:36:49 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
-
 import "./App.css";
 import Alcohol from "./components/alcohol/Alcohol.js";
 import RequireAuth from "./components/auth/RequireAuth";
-import SignIn from "./components/auth/SignIn";
+import SignIn from "./components/auth/signIn/SignIn";
 import Food from "./components/food/Food";
 import Glucose from "./components/glucose/Glucose";
 import Profile from "./components/profile/Profile";
@@ -27,6 +26,7 @@ function App() {
       <CssBaseline />
       <Routes>
         <Route path="/login" element={<SignIn />} />
+        <Route path="/signIn" element={<SignIn />} />
         <Route path="/" element={<SignIn />}></Route>
 
         <Route element={<RequireAuth />}>
