0be855f5fac8f10df498f70cbaf6ce4805817854
lettrealisa <konanhayumi.kh@gmail.com>
2022-12-30 01:24:31 +0300

    redux reauth

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,14 +1,50 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../auth/authSlice";
 
-const baseQuery = fetchBaseQuery({ baseUrl: "http://localhost:8080" });
+const baseQuery = fetchBaseQuery({
+  baseUrl: "http://localhost:8080",
+  prepareHeaders: (headers, { getState }) => {
+    //const token = getState().auth.token;
+    const token =
+      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjMwMDAiLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwODAiLCJleHAiOjE2NzIzNTI2ODcsInVzZXJuYW1lIjoiVGVzdCJ9.fsMvIZ7FDkmRBLBI2LmBYZzZ8wfcz_gtKbCyqJ7DZ24";
+
+    console.log(token);
+
+    if (token) {
+      headers.set("Authorization", `Bearer ${token}`);
+    }
+
+    return headers;
+  },
+});
 const baseQueryWithReauth = async (args, api, extraOptions) => {
   let result = await baseQuery(args, api, extraOptions);
   if (result.error && result.error.status === 401) {
-    const refreshResult = await baseQuery("/refreshToken", api, extraOptions);
+    const refreshResult = await baseQuery(
+      {
+        url: "/refreshToken",
+        method: "POST",
+        body: {
+          name: "Test",
+          age: 45,
+          job: "test",
+          pet: "test",
+          date: "2022-12-06T12:32",
+          role: {
+            name: "doctor",
+          },
+        },
+      },
+      api,
+      extraOptions
+    );
+    console.log("data");
+    console.log(refreshResult.data);
     if (refreshResult.data) {
       api.dispatch(setCredentials(refreshResult.data));
       result = await baseQuery(args, api, extraOptions);
+      console.log("result");
+      console.log(result);
     } else {
       //api.dispatch(loggedOut())
       console.log("Logged out");
